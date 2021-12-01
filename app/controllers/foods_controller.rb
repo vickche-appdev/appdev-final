9a1a00fe29b4a577e83694494f8d69ce13bcb8e6
vickche <vickyc@uchicago.edu>
2021-12-01 07:43:34 +0000

    happy with where this is at... working out most of the kinks, just need to figure out API and some formatting stuff. Looking okay though. and debug (for example, 0 meals left still lets you get the meal)

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -64,4 +64,32 @@ class FoodsController < ApplicationController
 
     redirect_to("/foods", { :notice => "Food deleted successfully."} )
   end
+
+
+  def location
+    # Parameters: {"user_street_address"=>"Merchandise Mart, Chicago"}
+
+    @street_address = params.fetch("user_street_address")
+
+    @maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address + "&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY"
+
+    @raw_json_string = open(@maps_url).read
+
+    @parsed_json = JSON.parse(@raw_json_string)
+
+    @results_array = @parsed_json.fetch("results")
+
+    @first_result = @results_array.at(0)
+    
+    @geometry_hash = @first_result.fetch("geometry")
+
+    @location_hash = @geometry_hash.fetch("location")
+
+    @latitude = @location_hash.fetch("lat")
+    @longitude = @location_hash.fetch("lng")
+
+    render({ :template => "foods/index.html.erb" })
+
+end
+
 end
